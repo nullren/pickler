1b495441bf3a479aba32257f79d7cbee217569f6
Renning Bruns <ren@renmail.net>
2024-11-17 16:27:21 -0800

    greet people every 2s

--- src/hello.rs
+++ src/hello.rs
@@ -1,10 +1,11 @@
-use bevy::prelude::IntoSystemConfigs;
+use bevy::prelude::{IntoSystemConfigs, Res, ResMut, Resource, Time, Timer, TimerMode};
 use bevy::prelude::{App, Commands, Component, Plugin, Query, Startup, Update, With};
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
     }
@@ -20,15 +21,22 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
